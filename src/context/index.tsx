ca43199220af5892f557e981340efdf13e76613f
momo-dev1 <didy1234567@gmail.com>
2022-11-01 17:14:32 +0200

    adding global context

---
+++ src/context/index.tsx
@@ -0,0 +1,63 @@
+import { createContext, FC, useContext } from "react";
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "t1", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "t2", text: "Learn TypeScript" }],
+    },
+    {
+      id: "3",
+      text: "Done",
+      tasks: [{ id: "t3", text: "Begin to use static typing" }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  lists: List[];
+  getTasksByListId(id: String): Task[];
+};
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+export const AppStateProvider: FC = ({ children }) => {
+  const { lists } = appData;
+
+  const getTasksByListId = (id: String) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+export const useAppGlobalState = () => {
+  return useContext(AppStateContext);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import { App } from "./App"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { AppStateProvider } from "./context";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
