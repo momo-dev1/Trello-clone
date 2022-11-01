71f0bd8421e79885817771b1def980d987fbcfcd
momo-dev1 <didy1234567@gmail.com>
2022-11-01 14:50:09 +0200

    add on Enter

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useFocus } from "../utils/useFocus";
 import {
   NewItemFormContainer,
@@ -14,12 +14,16 @@ const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") onAdd(text);
+  };
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
         onChange={({ target }) => setText(target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
