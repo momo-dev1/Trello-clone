184777680fd9805227ddda0554f2476965053b25
momo-dev1 <didy1234567@gmail.com>
2022-11-01 17:15:15 +0200

    adding taksk

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,22 @@
+import { useAppGlobalState } from "../context";
 import { ColumnContainer, ColumnTitle } from "../styles/styles";
 import AddNewItem from "./AddNewItem";
 import Card from "./Card";
 
 type ColumnProps = {
+  id: string;
   text: string;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ id, text }: ColumnProps) => {
+  const { getTasksByListId } = useAppGlobalState();
+  const tasks = getTasksByListId(id);
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate app scaffold"></Card>
-      <Card text="Learn TypeScript"></Card>
-      <Card text="Begin to use static typing"></Card>
+      {tasks.map((task) => (
+        <Card key={task.id} id={task.id} text={task.text} />
+      ))}
       <AddNewItem
         toggleButtonText="+ add new card"
         onAdd={() => console.log("newadd")}
