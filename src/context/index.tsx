177dfac132ee1be2f5090a1b430a82b5df19a5e1
momo-dev1 <didy1234567@gmail.com>
2022-11-05 19:51:01 +0200

    refactor

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,19 +1,5 @@
 import { createContext, FC, useContext } from "react";
-
-type Task = {
-  id: string;
-  text: string;
-};
-
-type List = {
-  id: string;
-  text: string;
-  tasks: Task[];
-};
-
-export type AppState = {
-  lists: List[];
-};
+import { Task, List, AppState } from "./reducer";
 
 const appData: AppState = {
   lists: [
